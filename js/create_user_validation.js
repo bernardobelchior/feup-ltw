faed8f9e1ae5ade089a8ceeed342b6cb344560e4
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-11-25 16:14:17 +0000

    Added js form validation.

---
+++ js/create_user_validation.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    let output = $('#output');
+    let username = $('#username').val();
+
+    if (username.length < 8) {
+        output.html('A username needs to be at least 8 characters long.');
+        return false;
+    }
+
+    let password = $('#password').val();
+
+    if (password.length < 7) {
+        output.html('A password needs to be at least 7 characters long.');
+        return false;
+    }
+
+    let passwordRepeat = $('#password-repeat').val();
+
+    if (password !== passwordRepeat) {
+        output.html('The provided passwords do not match.');
+        return false;
+    }
+
+    let email = $('#email').val();
+
+    if (false) { //TODO: Email regex to check if it is valid.
+        output.html('The provided email is not valid.\nPlease provide a valid one.');
+        return false;
+    }
+
+    this.submit();
+}
--- pages/templates/sign_up.php
+++ pages/templates/sign_up.php
@@ -1,15 +1,19 @@
-<form action="database/create_user.php" method="post">
-    <input type="text" name="username" placeholder="Username"/>
-    <input type="password" name="password" placeholder="Password"/>
-    <input type="email" name="email" placeholder="Email"/>
-    <input type="text" name="name" placeholder="Name"/>
-    <input type="date" name="birthdate" placeholder="Date of Birth"/>
+<script src="js/create_user_validation.js"></script>
 
-    <select name="gender">
+<form id="#form" method="post" action="database/create_user.php" onsubmit="return validateForm();">
+    <input id="username" type="text" name="username" placeholder="Username" required/>
+    <input id="password" type="password" name="password" placeholder="Password" required/>
+    <input id="password-repeat" type="password" name="password-repeat" placeholder="Repeat your Password" required/>
+    <input id="email" type="email" name="email" placeholder="Email" required/>
+    <input id="name" type="text" name="name" placeholder="Name" required/>
+    <input id="date" type="date" name="birthdate" placeholder="Date of Birth"/>
+
+    <select id="gender" name="gender">
         <option value="M">Male</option>
         <option value="F">Female</option>
     </select>
 
     <!-- Upload picture -->
     <button type="submit">Submit</button>
+    <span id="output"></span>
 </form>
