10ad54267b80e2b5a010dcaa78f8e38d3870003d
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-11-28 16:59:22 +0000

    Pages now use minified css.

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -7,7 +7,7 @@
                 src="https://code.jquery.com/jquery-3.1.1.min.js"
                 integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
                 crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="/css/header.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/header.min.css"/>
     </head>
 <body>
 
