02faaa5ebd0b36278372ffeba14a7b468931c787
epassos26 <epassos26@gmail.com>
2016-11-30 18:16:46 +0000

    Progress on edit_profile page, starting work on user profile page

--- pages/templates/edit_profile.php
+++ pages/templates/edit_profile.php
@@ -13,27 +13,55 @@ else {
 ?>
 <script type="text/javascript" src="../js/edit_profile.js"></script>
 
-<header id="page_title">
-  Edit Profile
-</header>
-<div id="general_info">
-    <div class="section_title">General Info</div>
+<div class="page_content">
+    <header class="page_title"><strong>Edit Profile</strong></header>
+    <div id="general_info">
+        <div class="section_title">General Info</div>
+    </div>
+    <ul id="profile_attr_list">
+        <li id="username">
+            <span class="list_attr_name"><strong>Username</strong></span>
+            <span class="list_attr_content"><?php echo getUserField($id, 'Username'); ?></span>
+            <span class="edit_link">Edit</span>
+        </li>
+        <li id="name">
+            <span class="list_attr_name"><strong>Name</strong></span>
+            <span class="list_attr_content"><?php echo getUserField($id, 'Name'); ?></span>
+            <span class="edit_link">Edit</span>
+        </li>
+        <li id="gender">
+            <span class="list_attr_name"><strong>Gender</strong></span>
+            <span class="list_attr_content"><?php echo getUserField($id, 'Gender'); ?></span>
+            <span class="edit_link">Edit</span>
+        </li>
+        <li id="email">
+            <span class="list_attr_name"><strong>e-mail</strong></span>
+            <span class="list_attr_content"><?php echo getUserField($id, 'Email'); ?></span>
+            <span class="edit_link">Edit</span>
+        </li>
+        <li id="dob">
+            <span class="list_attr_name"><strong>Date of Birth</strong></span>
+            <span class="list_attr_content"><?php echo getUserField($id, 'DateOfBirth'); ?></span>
+            <span class="edit_link">Edit</span>
+        </li>
+
+    </ul>
+<!--    <form id="form" method="post" action="actions/edit_profile.php" onsubmit="return validateDate();">-->
+<!--        <label id="username"> --><?php //echo getUserField($id, 'Username'); ?>
+<!--        </label>-->
+<!--        <input id="name" type="text" name="name" value="--><?php //echo getUserField($id, 'Name'); ?><!--"/>-->
+<!--        <input id="email" type="email" name="email" value="--><?php //echo getUserField($id, 'Email'); ?><!--"/>-->
+<!--        <input id="date" type="text" name="date" placeholder="yyyy-mm-dd"-->
+<!--               value="--><?php //echo getUserField($id, 'DateOfBirth'); ?><!--"/>-->
+<!---->
+<!--        <select id="gender" name="gender" gender=--><?php //echo getUserField($id, 'Gender'); ?><!-->-->
+<!--            <option value="M">Male</option>-->
+<!--            <option value="F">Female</option>-->
+<!--        </select>-->
+<!---->
+<!--        <button type="submit">Update</button>-->
+<!--        <span id="output"></span>-->
+<!--    </form>-->
+<!---->
+<!--    <button id="change_password">Change Password</button>-->
 </div>
-<form id="form" method="post" action="actions/edit_profile.php" onsubmit="return validateDate();">
-    <label id="username"> <?php echo getUserField($id, 'Username'); ?>
-    </label>
-    <input id="name" type="text" name="name" value="<?php echo getUserField($id, 'Name'); ?>"/>
-    <input id="email" type="email" name="email" value="<?php echo getUserField($id, 'Email'); ?>"/>
-    <input id="date" type="text" name="date" placeholder="yyyy-mm-dd"
-           value="<?php echo getUserField($id, 'DateOfBirth'); ?>"/>
-
-    <select id="gender" name="gender" gender=<?php echo getUserField($id, 'Gender'); ?>>
-        <option value="M">Male</option>
-        <option value="F">Female</option>
-    </select>
-
-    <button type="submit">Update</button>
-    <span id="output"></span>
-</form>
-
-<button id="change_password">Change Password</button>
--- pages/templates/profile.php
+++ pages/templates/profile.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="../css/profile.min.css">
+<link rel="stylesheet" href="../css/common.min.css">
 
 <?php
 include_once('../database/users.php');
@@ -15,43 +16,48 @@ $email = getUserField($id, 'Email');
 $name = getUserField($id, 'Name');
 
 ?>
-<div id="profile" class="container">
-    <div id="profile-header">
-        <div id="user-identification">
-            <span id="name">
-                <?php echo $name; ?>
-            </span>
-            <span id="username">
-                (<?php echo $username; ?>)
-            </span>
+<div class="page_content">
+    <header>
+        <span id="user_picture"></span>
+        <div id="user_realname">
+            <strong>
+                <?php echo $name?>
+            </strong>
+        </div>
+        <div id="user_username">
+            <?php echo $username ?>
+        </div>
+    </header>
+    <div id="profile" class="container">
+        <div id="profile-header">
+
+            <?php
+            if (groupIdHasPermissions((int)$_SESSION['groupId'], 'EDIT_ANY_PROFILE') || (int)$_SESSION['userId'] === $id) {
+                echo '<div id="edit-profile">';
+                echo '<a href="edit_profile.php?id=' . $id . '"><button>Edit Profile</button></a>';
+                echo '</div>';
+            }
+            ?>
         </div>
 
+        <div>
+            <span id="email">Email: </span>
+            <span> <?php echo $email; ?></span>
+        </div>
+
+        <div class="image">
+            <img src="" alt="User profile picture"/>
+        </div>
+
+    </div>
+
+    <div class="container" id="restaurants">
         <?php
-        if (groupIdHasPermissions((int)$_SESSION['groupId'], 'EDIT_ANY_PROFILE') || (int)$_SESSION['userId'] === $id) {
-            echo '<div id="edit-profile">';
-            echo '<a href="edit_profile.php?id=' . $id . '"><button>Edit Profile</button></a>';
-            echo '</div>';
+        if (groupIdHasPermissions($_SESSION['groupId'], 'ADD_ANY_RESTAURANT') || (int)$_SESSION['userId'] === $id) {
+            echo '<a href="add_restaurant.php?id=' . $id . '">';
+            echo '<button type="submit">Add Restaurant</button>';
+            echo '</a>';
         }
         ?>
     </div>
-
-    <div>
-        <span id="email">Email: </span>
-        <span> <?php echo $email; ?></span>
-    </div>
-
-    <div class="image">
-        <img src="" alt="User profile picture"/>
-    </div>
-
-</div>
-
-<div class="container" id="restaurants">
-    <?php
-    if (groupIdHasPermissions($_SESSION['groupId'], 'ADD_ANY_RESTAURANT') || (int)$_SESSION['userId'] === $id) {
-        echo '<a href="add_restaurant.php?id=' . $id . '">';
-        echo '<button type="submit">Add Restaurant</button>';
-        echo '</a>';
-    }
-    ?>
 </div>
