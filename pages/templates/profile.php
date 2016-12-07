2dbed95fd3ebc466d34bfc673c1c05ccd1b5e3ef
epassos26 <epassos26@gmail.com>
2016-12-07 15:58:22 +0000

    Started working on profile page

--- pages/templates/profile.php
+++ pages/templates/profile.php
@@ -25,54 +25,55 @@ if ($profile_picture === null)
 <link rel="stylesheet" href="../css/common.min.css">
 <script type="application/javascript" src="../js/common.js"></script>
 
-<div id="profile" class="container">
-    <div>
-        <div id="name">
-            <?php echo $name ?>
+<div class="page_content">
+    <div id="profile" class="container">
+        <div>
+            <div id="name">
+                <?php echo $name ?>
+            </div>
+
+            <div id="username">
+                <?php echo $username ?>
+            </div>
+
+            <span id="email">Email: </span>
+            <span> <?php echo $email; ?></span>
         </div>
 
-        <div id="username">
-            <?php echo $username ?>
-        </div>
+        <div>
+            <?php
+            if (groupIdHasPermissions((int)$_SESSION['groupId'], 'EDIT_ANY_PROFILE') || (int)$_SESSION['userId'] === $id)
+                echo '<a id="edit-profile" href="index.php?page=edit_profile.php&id=' . $id . '"><button>Edit Profile</button></a>';
+            ?>
 
-        <span id="email">Email: </span>
-        <span> <?php echo $email; ?></span>
+            <img id="profile-picture" src="<?php echo '../' . $profile_picture; ?>" alt="User profile picture"/>
+        </div>
     </div>
 
-    <div>
+
+    <div class="container" id="restaurants">
         <?php
-        if (groupIdHasPermissions((int)$_SESSION['groupId'], 'EDIT_ANY_PROFILE') || (int)$_SESSION['userId'] === $id)
-            echo '<a id="edit-profile" href="index.php?page=edit_profile.php&id=' . $id . '"><button>Edit Profile</button></a>';
+
+        /* Show all the restaurant the user has. */
+        $restaurants = getUserRestaurants($_SESSION['userId']);
+
+        foreach ($restaurants as $restaurant) {
+            echo '<div class="restaurant-container" onclick="openRestaurantProfile(' . $restaurant['ID'] . ')">';
+            // echo image
+            echo '<span>' . (string)$restaurant['Name'] . '</span>';
+            echo '<span>' . (string)$restaurant['Address'] . '</span>';
+            echo '</div>';
+        }
+
+        /* Shows the add restaurant button if the user has permission to add a restaurant. */
+        if (groupIdHasPermissions($_SESSION['groupId'], 'ADD_ANY_RESTAURANT') || (int)$_SESSION['userId'] === $id) {
+            $_SESSION['ownerId'] = $id;
+            echo '<div class="restaurant-container">';
+            echo '<a href="index.php?page=add_restaurant.php">';
+            echo '<button type="submit">Add Restaurant</button>';
+            echo '</a>';
+            echo '</div>';
+        }
         ?>
-
-        <img id="profile-picture" src="<?php echo '../' . $profile_picture; ?>" alt="User profile picture"/>
     </div>
-</div>
-
-</div>
-
-<div class="container" id="restaurants">
-    <?php
-
-    /* Show all the restaurant the user has. */
-    $restaurants = getUserRestaurants($_SESSION['userId']);
-
-    foreach ($restaurants as $restaurant) {
-        echo '<div class="restaurant-container" onclick="openRestaurantProfile(' . $restaurant['ID'] . ')">';
-        // echo image
-        echo '<span>' . (string)$restaurant['Name'] . '</span>';
-        echo '<span>' . (string)$restaurant['Address'] . '</span>';
-        echo '</div>';
-    }
-
-    /* Shows the add restaurant button if the user has permission to add a restaurant. */
-    if (groupIdHasPermissions($_SESSION['groupId'], 'ADD_ANY_RESTAURANT') || (int)$_SESSION['userId'] === $id) {
-        $_SESSION['ownerId'] = $id;
-        echo '<div class="restaurant-container">';
-        echo '<a href="index.php?page=add_restaurant.php">';
-        echo '<button type="submit">Add Restaurant</button>';
-        echo '</a>';
-        echo '</div>';
-    }
-    ?>
 </div>
