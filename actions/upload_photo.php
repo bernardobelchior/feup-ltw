567800db6af79c9e54f7ee8ef0696cde957319f0
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-12-15 10:43:09 +0000

    Final commit!

--- actions/upload_photo.php
+++ actions/upload_photo.php
@@ -3,7 +3,6 @@ session_start(['cookie_httponly' => true]);
 
 include_once('../utils/utils.php');
 include_once('../database/users.php');
-require_once('../utils/ImageManipulator.php');
 include_once('../database/users.php');
 
 // If the user didn't come from a valid page.
@@ -39,23 +38,10 @@ if ($oldPicture !== null)
 $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
 
 $file = $_FILES['photo']['tmp_name'];
-$manipulator = new ImageManipulator($file);
-$width = $manipulator->getWidth();
-$height = $manipulator->getHeight();
-$centerX = round($width / 2);
-$centerY = round($height / 2);
-
-//for 256x256 img
-$x1_256 = $centerX - 128;
-$y1_256 = $centerY - 128;
-
-$x2_256 = $centerX + 128;
-$y2_256 = $centerY + 128;
-
-$newImage = $manipulator->crop($x1_256, $y1_256, $x2_256, $y2_256);
 $picturePath = 'profile_pictures/' . $id . '.' . $extension;
-$manipulator->save('../' . $picturePath);
-changeProfilePicture($id, $picturePath);
+
+if (move_uploaded_file($file, '../' . $picturePath))
+    changeProfilePicture($id, $picturePath);
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 die();
